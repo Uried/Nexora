33a39cd95c496cba292e9f44634ec73d6563c863
Uried Fongang <uriedfongang4@gmail.com>
2026-01-23 10:55:40 +0100

    feat: add production API domain to Next.js image configuration

--- next.config.ts
+++ next.config.ts
@@ -9,6 +9,11 @@ const nextConfig: NextConfig = {
         port: '8000',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'api.kasi.market',
+        pathname: '/**',
+      },
     ],
   },
 };
